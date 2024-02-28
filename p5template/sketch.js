0e58dfbeb5a3911e4f6711f79eece5cf18c89389
ammeee3 <zvotebts@gmail.com>
2024-02-28 02:34:37 +0000

    edit sketch.js

--- p5template/sketch.js
+++ p5template/sketch.js
@@ -1,1 +1,43 @@
-//this is a test
+//sprites
+let playerSprite;
+let enemySprite;
+
+function preload() {
+
+}
+
+function setup() {
+
+}
+
+function draw() {
+
+}
+
+class Player{
+  constructor(sprite, x, y, sizeX, sizeY, speed){
+    this.sprite = sprite;
+    this.x = x;
+    this.y = y;
+    this.sizeX - sizeX;
+    this.sizeY - sizeY;
+    this.speed - speed;
+  }
+  display() {
+    Image(this.sprite, this.x, this.y, this.sizeX, this.sizeY);
+  }
+}
+
+class Enemy{
+  constructor(sprite, x, y, size, size, speed){
+    this.sprite = sprite;
+    this.x = x;
+    this.y = y;
+    this.size - size;
+    this.speed - speed;
+  }
+  display() {
+    Image(this.sprite, this.x, this.y, this.size, this.size);
+  }
+}
+
