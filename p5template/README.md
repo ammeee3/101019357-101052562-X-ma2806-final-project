996f9f68d94affd653bae2a1a46148daf406073c
ammeee3 <zvotebts@gmail.com>
2024-03-01 22:03:02 +0000

    edited README.md and sketch.js

--- p5template/README.md
+++ p5template/README.md
@@ -1,1 +1,18 @@
 cave crawler
+
+BY AMY BARCLAY 2407860, SCOTT 2403536, EMILIA 101019357
+
+OVERVIEW
+Our idea for a game is a cave crawler, with the main goal of the player to escape by navigating throught the cave, using your flashlight to avoid traps and deter monsters
+
+INDIVIDUAL CONTRIBUTIONS
+
+THEMES
+
+GAME DESIGN
+At the current stage of our project, the player is able to move around 
+In the future, 
+
+NARRATIVE
+
+AESTHETIC
--- p5template/sketch.js
+++ p5template/sketch.js
@@ -1,9 +1,45 @@
 //sprites
 let playerSprite;
-let enemySprite;
+let tilemap = [];
+let numDown = 10;
+let numAcross = 7;
+let tileSize = 50;
+let textures = [];
+
+let graphicMap = [
+  [3, 3, 3, 4, 3, 3, 3], //1
+  [3, 0, 0, 0, 1, 0, 3], //2
+  [3, 0, 1, 2, 0, 0, 3], //3
+  [3, 0, 0, 0, 0, 0, 3], //4
+  [3, 2, 0, 0, 0, 2, 3], //5
+  [3, 0, 1, 0, 0, 0, 3], //6
+  [3, 0, 1, 2, 1, 0, 3], //7
+  [3, 0, 0, 0, 0, 0, 3], //8
+  [3, 2, 0, 0, 0, 2, 3], //9
+  [3, 3, 3, 3, 3, 3, 3]  //10
+]
+
+let tileRules = [
+  [3, 3, 3, 4, 3, 3, 3], //1
+  [3, 0, 0, 0, 1, 0, 3], //2
+  [3, 0, 1, 2, 0, 0, 3], //3
+  [3, 0, 0, 0, 0, 0, 3], //4
+  [3, 2, 0, 0, 0, 2, 3], //5
+  [3, 0, 1, 0, 0, 0, 3], //6
+  [3, 0, 1, 2, 1, 0, 3], //7
+  [3, 0, 0, 0, 0, 0, 3], //8
+  [3, 2, 0, 0, 0, 2, 3], //9
+  [3, 3, 3, 3, 3, 3, 3]  //10
+]
+
 
 function preload() {
-
+  textures[0] = loadImage("floor.png");
+  textures[1] = loadImage("rock.png");
+  textures[2] = loadImage("hole.png");
+  textures[3] = loadImage("wall.png");
+  textures[4] = loadImage("exit.png");
+  playerSprite = loadImage("player.png");
 }
 
 function setup() {
@@ -11,7 +47,16 @@ function setup() {
 }
 
 function draw() {
+  background(0);
+  for (let across = 0; across < numAcross; across++) {
+      for (let down = 0; down < numDown; down++) {
+          tilemap[across][down].display(); 
+          tilemap[across][down].debug(); 
+      }
+  }
 
+  player.display();
+  player.move();
 }
 
 class Player{
@@ -28,16 +73,23 @@ class Player{
   }
 }
 
-class Enemy{
-  constructor(sprite, x, y, size, size, speed){
-    this.sprite = sprite;
-    this.x = x;
-    this.y = y;
-    this.size - size;
-    this.speed - speed;
-  }
-  display() {
-    Image(this.sprite, this.x, this.y, this.size, this.size);
-  }
+function movePlayer() {
+  case 'w':
+    if (
+      playerCurrentIndex
+    )
 }
+
+//class Enemy{
+  //constructor(sprite, x, y, size, size, speed){
+    //this.sprite = sprite;
+    // this.x = x;
+    //this.y = y;
+    //this.size - size;
+    //this.speed - speed;
+  //}
+  //display() {
+    //Image(this.sprite, this.x, this.y, this.size, this.size);
+  //}
+//}
 
