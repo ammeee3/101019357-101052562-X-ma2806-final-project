6f23f778e84f40d8dbfd863133135074a0dbe94d
ammeee3 <zvotebts@gmail.com>
2024-03-16 15:24:34 +0000

    updated comments

--- loadlevel.js
+++ loadlevel.js
@@ -20,13 +20,13 @@ function loadLevel() {
   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]  //15
   ];
 
-  tileRules = [ //displaying the tile map
+  tileRules = [ //rules for the tile map
   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3], //1
   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3], //2
   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3], //3
   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3], //4
   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3], //5
-  [3, 3, 3, 3, 3, 3, 4, 3, 3, 3], //6 //numbers represent the type of tile that will be displayed at every position in the tilemap(eg a 0 will show a floor tile)
+  [3, 3, 3, 3, 3, 3, 4, 3, 3, 3], //6 //numbers represent how each tile will act (eg a 2 is a trap tile and will kill the player if stepped on)
   [3, 3, 3, 3, 0, 0, 0, 1, 0, 3], //7
   [3, 3, 3, 3, 0, 1, 2, 0, 0, 3], //8
   [3, 3, 3, 3, 0, 0, 0, 0, 0, 3], //9
@@ -59,7 +59,7 @@ function loadLevel() {
   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]  //15
   ];
 
-  tileRules = [ //displaying the tile map
+  tileRules = [ 
   [3, 3, 3, 3, 3, 3, 3, 3, 4, 3], //1
   [3, 0, 0, 0, 0, 2, 0, 1, 0, 3], //2
   [3, 1, 1, 2, 0, 0, 0, 1, 0, 3], //3
@@ -98,7 +98,7 @@ function loadLevel() {
   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]  //15
   ];
   
-  tileRules = [ //displaying the tile map
+  tileRules = [ 
   [3, 3, 3, 4, 3, 3, 3, 3, 3, 3], //1
   [3, 0, 1, 0, 1, 1, 2, 0, 0, 3], //2
   [3, 0, 2, 0, 2, 0, 0, 0, 2, 3], //3
