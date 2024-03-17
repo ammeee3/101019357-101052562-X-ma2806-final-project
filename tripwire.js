2f6c8f12cd94f0caba1b2d9da95cff38d23d9d9e
ammeee3 <zvotebts@gmail.com>
2024-03-17 03:33:48 +0000

    fix tilemap bug + added comments

--- tripwire.js
+++ tripwire.js
@@ -1,7 +1,9 @@
 function tripwire() {   
-    if (level == 0){
-        spawnX = 6;
-        spawnY = 13;
+    if (level == 0){ //first level
+        //sets spawn point for level
+        spawnX = 6; //sets x position
+        spawnY = 13; //sets y position
+        //what the player sees in the level
         graphicMap = [
         [3, 3, 3, 3, 3, 3, 3, 3, 3, 3], //1
         [3, 3, 3, 3, 3, 3, 3, 3, 3, 3], //2
@@ -19,6 +21,7 @@ function tripwire() {
         [3, 3, 3, 3, 2, 0, 0, 0, 2, 3], //14
         [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]  //15
         ];
+        //how the player interacts with the level
         tileRules = [ //rules for the tile map
         [3, 3, 3, 3, 3, 3, 3, 3, 3, 3], //1
         [3, 3, 3, 3, 3, 3, 3, 3, 3, 3], //2
@@ -36,7 +39,7 @@ function tripwire() {
         [3, 3, 3, 3, 2, 0, 0, 0, 2, 3], //14
         [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]  //15
         ]; 
-
+        //deactivates tripwires depending on how many plates have been stepped on
         if (wire == 1){ //if one pressure plate is pressed, deactivates the following tripwires
             graphicMap[9][5] = 6;
             graphicMap[9][6] = 6;
@@ -58,9 +61,11 @@ function tripwire() {
             tileRules[7][5] = 6;
         }
 
-    }   if (level == 1){
+    }   if (level == 1){ //second level
+        //new points
         spawnX = 2;
         spawnY = 13;
+        //new graphic/rule map
         graphicMap = [ //displaying the tile map
         [3, 3, 3, 3, 3, 3, 3, 3, 4, 3], //1
         [3, 7, 0, 0, 0, 2, 0, 1, 0, 3], //2
@@ -78,7 +83,6 @@ function tripwire() {
         [3, 3, 0, 0, 2, 7, 3, 3, 3, 3], //14
         [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]  //15
         ];
-
         tileRules = [ 
         [3, 3, 3, 3, 3, 3, 3, 3, 4, 3], //1
         [3, 7, 0, 0, 0, 2, 0, 1, 0, 3], //2
@@ -97,6 +101,7 @@ function tripwire() {
         [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]  //15
         ];
 
+        //pressure plates are reset between levels
         if (wire == 1){ //if one pressure plate is pressed, deactivates the following tripwires
             graphicMap[7][3] = 6;
             graphicMap[7][4] = 6;
@@ -114,9 +119,11 @@ function tripwire() {
             tileRules[2][8] = 6;
         }
         
-    }   if (level == 2){
+    }   if (level == 2){ //final level
+        //new spawn points
         spawnX = 8;
         spawnY = 13;
+        //new graphic/rule map
         graphicMap = [ //displaying the tile map
         [3, 3, 3, 4, 3, 3, 3, 3, 3, 3], //1
         [3, 7, 1, 0, 1, 1, 2, 0, 7, 3], //2
@@ -134,7 +141,6 @@ function tripwire() {
         [3, 7, 0, 0, 0, 0, 0, 0, 0, 3], //14
         [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]  //15
         ];
-        
         tileRules = [ 
         [3, 3, 3, 4, 3, 3, 3, 3, 3, 3], //1
         [3, 7, 1, 0, 1, 1, 2, 0, 7, 3], //2
@@ -153,14 +159,14 @@ function tripwire() {
         [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]  //15
         ];
 
-        if (wire == 1){ //if one pressure plate is pressed, deactivates the following tripwires
+        if (wire == 1){ //deactivates the tripwires, depending on how many pressure plates were stepped on
             graphicMap[12][6] = 6;
             graphicMap[12][7] = 6;
           
             tileRules[12][6] = 6;
             tileRules[12][7] = 6;
 
-        } else if (wire == 2){ //if two pressure plates are pressed, deactivates all tripwires
+        } else if (wire == 2){ //
             graphicMap[12][6] = 6;
             graphicMap[12][7] = 6;
             graphicMap[12][2] = 6;
@@ -171,8 +177,9 @@ function tripwire() {
             tileRules[12][7] = 6;
             tileRules[12][2] = 6;
             tileRules[12][3] = 6;
-   
-        }   else if (wire == 3){ //if two pressure plates are pressed, deactivates all tripwires
+            tileRules[12][4] = 6;
+
+        }   else if (wire == 3){ //
             graphicMap[12][6] = 6;
             graphicMap[12][7] = 6;
             graphicMap[12][2] = 6;
@@ -184,9 +191,10 @@ function tripwire() {
             tileRules[12][7] = 6;
             tileRules[12][2] = 6;
             tileRules[12][3] = 6;
+            tileRules[12][4] = 6;
             tileRules[7][7] = 6;
 
-        } else if (wire == 4){ //if two pressure plates are pressed, deactivates all tripwires
+        } else if (wire == 4){ //
             graphicMap[12][6] = 6;
             graphicMap[12][7] = 6;
             graphicMap[12][2] = 6;
@@ -200,10 +208,11 @@ function tripwire() {
             tileRules[12][7] = 6;
             tileRules[12][2] = 6;
             tileRules[12][3] = 6;
+            tileRules[12][4] = 6;
             tileRules[7][7] = 6;
             tileRules[4][2] = 6;
         
-        } else if (wire >= 5){ //if two pressure plates are pressed, deactivates all tripwires
+        } else if (wire >= 5){ //all tripwires deactivated - max 5 pressure plates
             graphicMap[12][6] = 6;
             graphicMap[12][7] = 6;
             graphicMap[12][2] = 6;
@@ -217,6 +226,7 @@ function tripwire() {
             tileRules[12][7] = 6;
             tileRules[12][2] = 6;
             tileRules[12][3] = 6;
+            tileRules[12][4] = 6;
             tileRules[7][7] = 6;
             tileRules[4][2] = 6;
             tileRules[2][3] = 6;
